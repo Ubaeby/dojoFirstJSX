2b751ca8e4a28979d6902b8a30d6fa843113f1ec
Ubaeby <christianchoi45@gmail.com>
2022-12-01 12:36:20 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className = "head">
+        <h1>Hello Dojo!</h1>
+        <h3>Things I need to do: </h3>
+      </header>
+
+      <ul className='list'>
+        <li>Learn React</li>
+        <li>Finish my assignments</li>
+        <li>Get a Black Belt</li>
+        <li>Become a SDE</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
